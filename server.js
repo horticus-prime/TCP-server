342edd8a4ebd1e426f8505de3389ff199ce1d41c
Michael A. Chapman <chapmanm305@gmail.com>
2019-06-24 16:35:42 -0700

    Initialize hello world

---
+++ server.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const io = require('socket.io')(3005);
+
+io.on('connection', socket => {
+  console.log(`Connection from: ${socket.id}`);
+
+  socket.on('moisture-data', payload => {
+    console.log('moisture-data');
+
+    io.emit('moisture-data', payload);
+  });
+
+  socket.on('req-data', payload => {
+    console.log('req-data');
+
+    io.emit('req-data', payload);
+  });
+});
+
+let helloWorld = () => {
+  console.log('Hello World');
+};
+
+helloWorld();
