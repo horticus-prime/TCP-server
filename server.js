9ea0db1cf5fce65fe6f7a78f5be1adf08d4c0552
Michael A. Chapman <chapmanm305@gmail.com>
2019-06-27 15:14:21 -0700

    Merge conflict

--- server.js
+++ server.js
@@ -6,7 +6,14 @@ io.on('connection', socket => {
   console.log(`Connection from: ${socket.id}`);
 
   socket.on('moisture-data', payload => {
-    io.emit('moisture-data', payload);
+    console.log('PAYLOAD: ', payload);
+    let dataObj = {
+      timestamp: new Date(),
+      moistureCategory: payload.result,
+      moistureNumber: payload.m,
+    };
+
+    io.emit('moisture-data', dataObj);
   });
 
   socket.on('req-data', payload => {
