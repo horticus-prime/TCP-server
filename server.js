de8aa6fe5a75e180f91a9eb75129f6ae115d59ac
Michael A. Chapman <chapmanm305@gmail.com>
2019-06-27 11:20:24 -0700

    Cleanup code and remove unecessary code

--- server.js
+++ server.js
@@ -1,29 +1,25 @@
 'use strict';
 
-var app = require('express')();
-var server = require('http').Server(app);
-const io = require('socket.io')(server);
-
-server.listen(process.env.PORT);
+const io = require('socket.io')(3030);
 
 io.on('connection', socket => {
   console.log(`Connection from: ${socket.id}`);
 
   socket.on('moisture-data', payload => {
-    console.log('moisture-data');
-
     io.emit('moisture-data', payload);
   });
 
   socket.on('req-data', payload => {
-    console.log('req-data');
-
     io.emit('req-data', payload);
+  });
+
+  socket.on('save-status', payload => {
+    io.emit('save-status', payload);
   });
 });
 
-let helloWorld = () => {
-  console.log('Hello World');
-};
+// let helloWorld = () => {
+//   console.log('Hello World');
+// };
 
-helloWorld();
+// helloWorld();
