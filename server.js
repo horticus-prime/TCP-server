0a0fdac0c125480e639d590e30597cd6f9e865a6
Michael A. Chapman <chapmanm305@gmail.com>
2019-06-27 15:50:24 -0700

    Add console.log

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ io.on('connection', socket => {
   console.log(`Connection from: ${socket.id}`);
 
   socket.on('moisture-data', payload => {
+    console.log(typeof payload);
     let newPayload = JSON.parse(payload);
     console.log('PAYLOAD: ', JSON.parse(payload));
     console.log(newPayload.category);
