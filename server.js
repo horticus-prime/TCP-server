b389ab22189d50dcd5735c0fbe337ab67f0575b3
Michael A. Chapman <chapmanm305@gmail.com>
2019-07-30 11:31:02 -0700

    Implement cron functionality - Mike, Chloie

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 'use strict';
 
-var app = require('express')();
-var server = require('http').Server(app);
-const io = require('socket.io')(server);
+let cron = require('node-cron');
 
-server.listen(process.env.PORT);
+// var app = require('express')();
+// var server = require('http').Server(app);
+// const io = require('socket.io')(server);
+const io = require('socket.io')(3006);
+
+// server.listen(process.env.PORT);
+
+let arr = [];
 
 io.on('connection', socket => {
   console.log(`Connection from: ${socket.id}`);
 
+  // emit data
   socket.on('moisture-data', payload => {
     let newPayload = JSON.parse(payload);
+    arr.push(newPayload);
     
     let dataObj = {
       timestamp: new Date(),
@@ -19,6 +26,13 @@ io.on('connection', socket => {
     };
 
     io.emit('moisture-data', dataObj);
+  });
+
+  // database data
+  cron.schedule('*/1 * * * *', function() {
+    console.log('array', arr);
+    io.emit('database-data', arr[arr.length-1]);
+    arr = [];
   });
 
   socket.on('req-data', payload => {
